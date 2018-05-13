18c12144b57b0de763e75b802a94cf403c97b3d0
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2018-05-13 18:09:30 +0200

    Cannot find javadoc comments anymore

--- src/main/java/it/polimi/ingsw/model/GameBoard.java
+++ src/main/java/it/polimi/ingsw/model/GameBoard.java
@@ -1,71 +1,113 @@
 package it.polimi.ingsw.model;
+import it.polimi.ingsw.model.exception.*;
 
-import it.polimi.ingsw.model.achievement.*;
-import it.polimi.ingsw.model.exception.NotValidValueException;
+import java.util.List;
+import java.util.ArrayList;
 
-import java.util.*;
+public class RoundTrack {
+    private static final int NUMBER_OF_ROUNDS = 10;
+    private ArrayList[] diceList;
 
-public class GameBoard {
-    private static final int NUMBER_OF_PUBLIC_ACHIEVEMENTS = 3;
-    private RoundTrack roundTrack;
-    private ArrayList<Player> players = new ArrayList<>();
-    private DiceBag diceBag;
-    private ArrayList<CardAchievement> publicAchievements = new ArrayList<>();
-    private ArrayList<Die> draftPool = new ArrayList<>();
+    /**
+     * Creates a RoundTrack object which represents the round track on the game board.
+     * The round track has as many cells as the number of rounds.
+     * At the end of each round, the remaining dice from the dice stock are placed on the cell representing the just ended round.
+     * Round track's cells are represented as ArrayLists of Die objects.
+     */
+    public RoundTrack() {
+        diceList = new ArrayList[NUMBER_OF_ROUNDS];
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++)
+            diceList[i] = new ArrayList<Die>();
+    }
 
-    public GameBoard (Map<String, String> schemes) throws NotValidValueException {
+    /**
+     * Returns a Die objects ArrayList, representing the list of dice placed on a round track's cell.
+     * The argument must specify which cell the user wants to check.
+     * @param round the round track's cell that the user wants to check
+     * @return an ArrayList containing the dice placed on the selected cell
+     * @throws NotValidRoundException whether the user tries to access a not existing round track's cell
+     */
+    public List getDiceList(int round) throws NotValidRoundException {
+        if( round > 0 && round <= NUMBER_OF_ROUNDS)
+            return diceList[round-1];
+        else throw new NotValidRoundException();
+    }
 
-        this.roundTrack = new RoundTrack();
-        this.diceBag = new DiceBag();
+    /**
+     * Allows the user to place the dice remaining in the dice stock (at the end of each round) on the selected cell.
+     * The argument must specify which cell the user wants to place the dice on.
+     * @param mySet the dice set the user wants to place
+     * @param round the cell the user wants to place the dice on
+     * @throws NotValidRoundException whether the user specifies a not existing round track's cell
+     */
+    public void setDiceList(ArrayList<Die> mySet, int round) throws NotValidRoundException {
+        if ( round > 0 && round <= NUMBER_OF_ROUNDS)
+            diceList[round - 1] = mySet;
+        else throw new NotValidRoundException();
+    }
 
-        List<PublicAchievementNames> publicAchievementList = Arrays.asList(PublicAchievementNames.values());
-        Collections.shuffle(publicAchievementList);
-        AbstractCardFactory abstractFactory = new CardFactory();
-        for ( int i = 0; i < NUMBER_OF_PUBLIC_ACHIEVEMENTS; i++ ) {
-            CardAchievement publicAchievementsFactory = abstractFactory.extractCardAchievement(publicAchievementList.get(i));
-           this.publicAchievements.add(publicAchievementsFactory);
+    /**
+     * Returns an int representing the round which is currently played.
+     * As the game allows you to put dice on the round's cell only at the end of the round, we're sure that the first empty cell
+     * represents the currently played round.
+     * @return the currently played round
+     * @throws EndedGameException if every round has been already played
+     */
+    public int getCurrentRound() throws EndedGameException {
+        int roundIndex = 0;
+        while(roundIndex < NUMBER_OF_ROUNDS) {
+            if (this.diceList[roundIndex].isEmpty())
+                return roundIndex+1;
+            roundIndex++;
         }
-
-        List<Color> privateAchievementsList = Arrays.asList(Color.values());
-        Collections.shuffle(privateAchievementsList);
-        int i = 0;
-
-        for ( Map.Entry<String, String> elem: schemes.entrySet() ) {
-            players.add(new Player(elem.getKey(), new Dashboard(elem.getValue()), new PrivateAchievement(privateAchievementsList.get(i))));
-            players.get(i).getDashboard().setOwner(players.get(i));
-            i++;
-        }
+        throw new EndedGameException();
     }
 
-    public RoundTrack getRoundTrack() {
-        return roundTrack;
-    }
-
-    public DiceBag getDiceBag() {
-        return diceBag;
-    }
-
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
-
-    public ArrayList<CardAchievement> getPublicAchievements() {
-        return publicAchievements;
-    }
-
-    public ArrayList<Die> getDraftPool() {
-        return new ArrayList<>(this.draftPool);
-    }
-
-    public void setDraftPool(ArrayList<Die> draftPool) {
-        this.draftPool.addAll(draftPool);
-    }
-
-    public void removeDieFromDraftPool ( Die die ){
+    /**
+     * Allows the user to place the dice remaining in the dice stock (at the end of the round) on the first empty cell (representing the currently played round).
+     * This also marks the end of that round.
+     * @param mySet the dice set the user wants to place on the cell
+     * @throws NotValidRoundException if the user tries to access the round track after the game is ended (every round already played)
+     */
+    public void setNextRound(ArrayList<Die> mySet) throws NotValidRoundException {
+        int currentRound = 0;
         try {
-            this.draftPool.remove(draftPool.indexOf(die));
-        }catch (Exception e) {
+            currentRound = this.getCurrentRound();
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
+        if ( currentRound > 0 && currentRound <= NUMBER_OF_ROUNDS )
+            this.setDiceList(mySet, currentRound);
+        else throw new NotValidRoundException();
+    }
+
+    /**
+     * Checks whether there is a die on the round track having the color chosen
+     * as argument by the user.
+     * @param color the color of which the user wants to know whether there is a die on the round track or not
+     * @return a Boolean object specifying if there is a die having that color or not
+     */
+    public Boolean isColorOnRoundTrack (Color color) {
+        for ( int i = 0; i < NUMBER_OF_ROUNDS; i++ )
+            for ( Object object: diceList[i]){
+                Die d = (Die) object;
+                if (d.getColor().equals(color))
+                    return true;
+            }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String myRoundTrack = "Round track:\n\n";
+        StringBuilder bld = new StringBuilder();
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+            bld.append("Round ");
+            bld.append(i + 1);
+            bld.append(" :\n");
+            for (Object die : diceList[i])
+                bld.append(die.toString());
+        }
+        return myRoundTrack + bld.toString();
     }
 }
