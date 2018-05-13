4fb6da3d97f44a99d38afd014748f60d79d13f82
ChristianCammareri <38110773+ChristianCammareri@users.noreply.github.com>
2018-05-13 15:37:49 +0200

    Placement Classes

--- src/main/java/it/polimi/ingsw/controller/Round.java
+++ src/main/java/it/polimi/ingsw/controller/Round.java
@@ -3,51 +3,53 @@ package it.polimi.ingsw.controller;
 import it.polimi.ingsw.model.Die;
 import it.polimi.ingsw.model.GameBoard;
 import it.polimi.ingsw.model.Player;
-import it.polimi.ingsw.model.exception.EndedGameException;
-import it.polimi.ingsw.model.exception.NotValidRoundException;
 
 import java.util.*;
 
-public class Round{
-    private ArrayList<Die> draftPool;
+public class Round {
     private static int DRAFT_POOL_CAPACITY;
+    private ArrayList<Die> draftPool;
     private Turn currentTurn;
 
-    public Round(List<Player> players){
-        DRAFT_POOL_CAPACITY = (players.size() * 2 ) + 1;
-        this.draftPool= new ArrayList<>(DRAFT_POOL_CAPACITY);
+    public Round(List<Player> players) {
+        DRAFT_POOL_CAPACITY = (players.size() * 2) + 1;
+        this.draftPool = new ArrayList<>(DRAFT_POOL_CAPACITY);
     }
 
-    public void initializeDraftPool(List<Player> players,GameBoard gameBoard) {
+    public void initializeDraftPool(List<Player> players, GameBoard gameBoard) {
         for (int i = 0; i < DRAFT_POOL_CAPACITY; i++)
             draftPool.add(gameBoard.getDiceBag().extract());
     }
 
-    public void roundManager(List<Player> players,GameBoard gameBoard){
+    public void roundManager(List<Player> players, GameBoard gameBoard) {
         ListIterator<Player> iterator = players.listIterator();
-        Map <Player,Boolean> secondTurnPlayed = new HashMap<>(players.size());
+        Map<Player, Boolean> secondTurnPlayed = new HashMap<>(players.size());
 
-        while(iterator.hasNext()){
-            this.currentTurn =new Turn(iterator.next(),gameBoard,false);
-            secondTurnPlayed.put(iterator.next(), currentTurn.isHasSecondTurn());
+        while (iterator.hasNext()) {
+            this.currentTurn = new Turn(iterator.next(), gameBoard, false);
+            Thread turn = new Thread(currentTurn);
+            turn.start();
+            while (!turn.isAlive())
+                secondTurnPlayed.put(iterator.next(), currentTurn.isHasSecondTurn());
         }
-        while (iterator.hasPrevious()){
-            if(secondTurnPlayed.get(iterator.next())) {
-               this.currentTurn= new Turn(iterator.next(),gameBoard,true);
+        while (iterator.hasPrevious()) {
+            if (secondTurnPlayed.get(iterator.next())) {
+                this.currentTurn = new Turn(iterator.next(), gameBoard, true);
+                Thread turn = new Thread(currentTurn);
+                turn.start();
             }
         }
     }
 
-   public void endRound (GameBoard gameBoard) throws NotValidRoundException, EndedGameException {
+    public void endRound(GameBoard gameBoard) {
         try {
             gameBoard.getRoundTrack().setNextRound(this.draftPool);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
 
 
-
-   }
+    }
 
     public Turn getCurrentTurn() {
         return currentTurn;
