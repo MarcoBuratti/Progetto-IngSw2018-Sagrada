8a1f865887682cbc66dddad19b77a4432e9748dc
sergiocanzoneri <sergiocanzoneri@mail.polimi.it>
2018-05-14 12:25:03 +0200

    Updates 14/05

--- src/main/java/it/polimi/ingsw/controller/socket/ProvaClient.java
+++ src/main/java/it/polimi/ingsw/controller/socket/ProvaClient.java
@@ -1,11 +1,8 @@
 package it.polimi.ingsw.controller.socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class ProvaClient {
-    public static void main (String[] args) throws IOException {
+    public static void main (String[] args) {
         Client client = new Client();
         client.run();
     }
