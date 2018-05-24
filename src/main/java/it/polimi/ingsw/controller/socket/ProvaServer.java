3ee8f123229f3fb24084f1bd34db5eccbce2de19
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2018-05-24 14:09:47 +0200

    Update

--- src/main/java/it/polimi/ingsw/controller/socket/ProvaServer.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.controller.socket;
-
-public class ProvaServer {
-    public static void main(String [] args) {
-        Server server = new Server();
-        server.run();
-    }
-}
--- src/main/java/it/polimi/ingsw/model/rmi/interfaces/MessengerInterface.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.model.rmi.interfaces;
-
-import java.rmi.*;
-
-public interface MessengerInterface  extends Remote{
-
-    String getUsername() throws RemoteException;
-    void tell(String s) throws RemoteException;
-}
