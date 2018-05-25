47fbfeb1a5100f0465ffaed4556d1f44c2400dc1
MarcoBuratti <37804834+MarcoBuratti@users.noreply.github.com>
2018-05-25 16:34:35 +0200

    Connection Update

--- src/main/java/it/polimi/ingsw/server/interfaces_and_abstract_classes/RmiServerInterface.java
+++ src/main/java/it/polimi/ingsw/server/interfaces_and_abstract_classes/RmiServerInterface.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.server.interfaces_and_abstract_classes;
 
+import it.polimi.ingsw.server.controller.action.PlayerMove;
 import it.polimi.ingsw.util.Message;
 
 import java.rmi.Remote;
@@ -7,5 +8,7 @@ import java.rmi.RemoteException;
 
 public interface RmiServerInterface extends Remote {
     void update(Message message) throws RemoteException; //IN REALTà POTRESTI USARE COME PARAMETRO IN INGRESSO LA PLAYERMOVE
-    void setPlayer(Message message);
+    void setPlayerAndAskScheme(Message message) throws RemoteException;
+    void setDashboard (Message message) throws RemoteException;
+    void sendMove (PlayerMove playerMove) throws RemoteException;
 }
--- src/main/java/it/polimi/ingsw/server/rmi/RmiConnectionServer.java
+++ src/main/java/it/polimi/ingsw/server/rmi/RmiConnectionServer.java
@@ -12,29 +12,68 @@ import java.rmi.ConnectException;
 import java.rmi.RemoteException;
 import java.util.Observable;
 
-public class RmiConnectionServer extends ServerAbstractClass implements RmiServerInterface {
+import it.polimi.ingsw.client.interfaces.RmiClientInterface;
+import it.polimi.ingsw.server.Server;
+import it.polimi.ingsw.server.controller.action.PlayerMove;
+import it.polimi.ingsw.server.interfaces_and_abstract_classes.RmiServerInterface;
+import it.polimi.ingsw.server.interfaces_and_abstract_classes.ServerAbstractClass;
+import it.polimi.ingsw.server.model.Player;
+import it.polimi.ingsw.server.model.exception.NotValidValueException;
+import it.polimi.ingsw.util.Message;
+
+import java.io.IOException;
+import java.rmi.ConnectException;
+import java.rmi.RemoteException;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.StringTokenizer;
+
+public class RmiConnectionServer extends ServerAbstractClass implements RmiServerInterface, Observer {
 
     Server server;
-    boolean isOn = true;
     private Player player;
     private RmiClientInterface client;
+    private boolean isYourTurn;
 
 
     public RmiConnectionServer(RmiClientInterface connectionClientRMI, Server server) {
         this.client = connectionClientRMI;
         this.server = server;
-        send("Attendere prego");
-        server.registerConnection(this);
     }
 
     @Override
-    public void setPlayer (Message message){
+    public void setPlayerAndAskScheme(Message message) throws RemoteException{
         this.setPlayer(new Player(message.getMessage(), this));
+        server.registerConnection(this);
+        try {
+            askForChosenScheme();
+        }catch (Exception e){
+            System.err.println(e.toString());
+        }
     }
 
     @Override
-    public void setPlayer(Player player) {
+    public void setDashboard(Message message) throws RemoteException {
+        try {
+            this.player.setDashboard(message.getMessage());
+        } catch (NotValidValueException e) {
+            System.err.println(e.toString());
+        }
+    }
 
+    public void notYourTurn () { send("It's not your turn! Please wait.");}
+
+    @Override
+    public void sendMove(PlayerMove playerMove) throws RemoteException {
+        if(getYourTurn()) {
+            setChanged();
+            notifyObservers(playerMove);
+        } else notYourTurn();
+    }
+
+    @Override
+    public synchronized void setPlayer(Player player) {
+        this.player = player;
     }
 
     @Override
@@ -43,19 +82,19 @@ public class RmiConnectionServer extends ServerAbstractClass implements RmiServe
     }
 
     @Override
-    public boolean getYourTurn() {
-        return false;
+    public synchronized boolean getYourTurn() {
+        return isYourTurn;
     }
 
     @Override
-    public void setYourTurn(boolean bool) {
-
+    public synchronized void setYourTurn(boolean bool) {
+        this.isYourTurn = bool;
     }
 
     @Override
-    public void send(String message) {
+    public void send(String string) {
         try{
-            client.update(message);
+            client.update(string);
         }catch(ConnectException e) {
             e.printStackTrace();
             System.out.println("Client rimosso! - 1");
@@ -73,19 +112,22 @@ public class RmiConnectionServer extends ServerAbstractClass implements RmiServe
 
     @Override
     public void close() {
-        isOn = false;
+
         send("Connessione terminata!");
         send("Terminate");
         server.deregisterConnection(this);
     }
 
-    public void receiveName (String name){
-        setPlayer(new Player(name, this));
-    }
 
-    @Override
-    public String askForChosenScheme() throws IOException {
-        return null;
+    public void askForChosenScheme() throws IOException {
+        StringBuilder bld = new StringBuilder();
+        bld.append(server.getSchemes().get(0).getFirstScheme() + "_" + server.getSchemes().get(0).getSecondScheme());
+        bld.append("_");
+        server.getSchemes().remove(0);
+        bld.append(server.getSchemes().get(0).getFirstScheme() + "_" + server.getSchemes().get(0).getSecondScheme());
+        String message = bld.toString();
+        server.getSchemes().remove(0);
+        this.send("Please choose one of these schemes in a minute: insert a number between 1 and 4.\n" + message);
     }
 
     public void update(Message message) throws RemoteException{ //NOTIFICA LA REMOTE VIEW
@@ -95,6 +137,7 @@ public class RmiConnectionServer extends ServerAbstractClass implements RmiServe
 
     @Override
     public void update(Observable o, Object arg) {
-        //E' CHIAMATO DA TURN
+        boolean bool = (boolean) arg;
+        this.setYourTurn(bool);
     }
 }
