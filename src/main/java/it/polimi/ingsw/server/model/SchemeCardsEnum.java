88ae1abe63e5cb136c0e9b19d9b2a02b20cf4d93
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2018-05-31 17:45:17 +0200

    Update

--- src/main/java/it/polimi/ingsw/server/model/SchemeCardsEnum.java
+++ src/main/java/it/polimi/ingsw/server/model/SchemeCardsEnum.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.server.model;
 
 public enum SchemeCardsEnum {
-    CARD1("Aurorae Magnificus", "Aurora Sagradis"), CARD2("Bellesguard", "Batllo"), CARD3("Comitas", "Chromatic Splendor"),
-    CARD4("Firelight", "Sun's Glory"), CARD5("Fractal Drops", "Ripples of Light"), CARD6("Fulgor del Cielo", "Luz Celestial"),
-    CARD7("Industria", "Via Lux"), CARD8("Kaleidoscopic Dream", "Firmitas"), CARD9("Lux Mundi", "Lux Astram"),
-    CARD10("Sun Catcher", "Shadow Thief"), CARD11("Virtus", "Symphony of Light"), CARD12("Water of Life", "Gravitas");
+    CARD1("Aurorae_Magnificus", "Aurora_Sagradis"), CARD2("Bellesguard", "Batllo"), CARD3("Comitas", "Chromatic_Splendor"),
+    CARD4("Firelight", "Sun's_Glory"), CARD5("Fractal_Drops", "Ripples_of_Light"), CARD6("Fulgor_del_Cielo", "Luz_Celestial"),
+    CARD7("Industria", "Via_Lux"), CARD8("Kaleidoscopic_Dream", "Firmitas"), CARD9("Lux_Mundi", "Lux_Astram"),
+    CARD10("Sun_Catcher", "Shadow_Thief"), CARD11("Virtus", "Symphony_of_Light"), CARD12("Water_of_Life", "Gravitas");
 
     private String firstScheme;
     private String secondScheme;
