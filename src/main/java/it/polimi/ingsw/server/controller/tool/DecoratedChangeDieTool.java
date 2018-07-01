e35f60ec2b374fbefde43f721ba2277d7b84a9b7
sergiocanzoneri <sergio.canzoneri@mail.polimi.it>
2018-07-01 10:43:53 +0200

    Fixing (Sergio Canzoneri & Christian Cammareri)

--- src/main/java/it/polimi/ingsw/server/controller/tool/DecoratedChangeDieTool.java
+++ src/main/java/it/polimi/ingsw/server/controller/tool/DecoratedChangeDieTool.java
@@ -1,11 +1,14 @@
 package it.polimi.ingsw.server.controller.tool;
 
 import it.polimi.ingsw.server.controller.Turn;
+import it.polimi.ingsw.server.controller.action.PlacementMove;
 import it.polimi.ingsw.server.controller.action.PlayerMove;
 import it.polimi.ingsw.server.model.Color;
 import it.polimi.ingsw.server.model.Die;
+import it.polimi.ingsw.server.model.exception.NotValidParametersException;
+import it.polimi.ingsw.server.model.exception.OccupiedCellException;
 
-public class DecoratedChangeDieTool extends PlaceToolDecorator {
+public class DecoratedChangeDieTool implements PlaceToolDecorator {
 
     private Tool myTool;
 
@@ -59,5 +62,28 @@ public class DecoratedChangeDieTool extends PlaceToolDecorator {
     @Override
     public ToolNames getToolName() {
         return myTool.getToolName();
+    }
+
+    @Override
+    public boolean placeDie(Turn turn, PlayerMove playerMove) {
+        if (playerMove.getIndexDie().isPresent()) {
+            try {
+                PlacementMove placementMove = new PlacementMove(turn.getPlayer(), playerMove.getIntParameters(0),
+                        playerMove.getIntParameters(1), turn.getGameBoard().getDraftPool().get(playerMove.getIndexDie().get()));
+                turn.setPlacementDone ( placementMove.placeDie() );
+                if ( turn.isPlacementDone() ) {
+                    turn.getGameBoard().removeDieFromDraftPool(placementMove.getDie());
+                    turn.sendToPlayer("The die has been placed on the selected cell.");
+                    return true;
+                } else {
+                    turn.sendToPlayer("Incorrect move! Please try again.");
+                    return false;
+                }
+            } catch (OccupiedCellException | NotValidParametersException e) {
+                return false;
+            }
+        } else
+            return false;
+
     }
 }
--- src/main/java/it/polimi/ingsw/server/controller/tool/DecoratedSetDieTool.java
+++ src/main/java/it/polimi/ingsw/server/controller/tool/DecoratedSetDieTool.java
@@ -1,10 +1,13 @@
 package it.polimi.ingsw.server.controller.tool;
 
 import it.polimi.ingsw.server.controller.Turn;
+import it.polimi.ingsw.server.controller.action.PlacementMove;
 import it.polimi.ingsw.server.controller.action.PlayerMove;
 import it.polimi.ingsw.server.model.Color;
+import it.polimi.ingsw.server.model.exception.NotValidParametersException;
+import it.polimi.ingsw.server.model.exception.OccupiedCellException;
 
-public class DecoratedSetDieTool extends PlaceToolDecorator {
+public class DecoratedSetDieTool implements PlaceToolDecorator {
     private Tool myTool;
 
     public DecoratedSetDieTool ( Tool tool ) {
@@ -57,5 +60,28 @@ public class DecoratedSetDieTool extends PlaceToolDecorator {
     @Override
     public ToolNames getToolName() {
         return myTool.getToolName();
+    }
+
+    @Override
+    public boolean placeDie(Turn turn, PlayerMove playerMove) {
+        if (playerMove.getIndexDie().isPresent()) {
+            try {
+                PlacementMove placementMove = new PlacementMove(turn.getPlayer(), playerMove.getIntParameters(0),
+                        playerMove.getIntParameters(1), turn.getGameBoard().getDraftPool().get(playerMove.getIndexDie().get()));
+                turn.setPlacementDone ( placementMove.placeDie() );
+                if ( turn.isPlacementDone() ) {
+                    turn.getGameBoard().removeDieFromDraftPool(placementMove.getDie());
+                    turn.sendToPlayer("The die has been placed on the selected cell.");
+                    return true;
+                } else {
+                    turn.sendToPlayer("Incorrect move! Please try again.");
+                    return false;
+                }
+            } catch (OccupiedCellException | NotValidParametersException e) {
+                return false;
+            }
+        } else
+            return false;
+
     }
 }
